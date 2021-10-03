2f5f17d771710c0b20816c90abc56ddf28f5a75c
Halwest Manguri <halwestkurd7@gmail.com>
2021-10-03 13:24:59 +0300

    initial commit

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2b30a818becc6a0ac34122dafbc750d5'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '82d8eeae-a025-4470-8496-8ab230f92366'
+};
